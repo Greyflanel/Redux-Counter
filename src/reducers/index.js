f0553c4f29527e45b0b259a9c008dc23e7188ccf
Greyflanel <richardlovelace811@gmail.com>
2019-05-07 15:33:08 -0500

    My first commit.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,13 +9,18 @@ const initialState = {
 // of our redux store, along with an action created
 // by our action creator. What does the reducer
 // need to do with the count in each case?
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
+    return initialState.count++
     // Fill in the body of this case
     case DECREMENT:
+    return initialState.count--
     // Fill in the body of this case
     default:
       return state;
   }
+  
+  
 };
